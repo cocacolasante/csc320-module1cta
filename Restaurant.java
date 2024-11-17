bacb7352962af2c0eed516ff9604609bcff89bc6
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2024-11-17 09:49:25 -0500

    init

---
+++ Restaurant.java
@@ -0,0 +1,15 @@
+public class Restaurant{
+    public static void main(String[] args){
+
+
+        String restaurantName ="Cheesesteak King";
+        String restAddy = "123 cheesewiz ln";
+        String restCity = "Philadelphia";
+        String restState = "PA";
+        String restZip = "19003";
+
+        System.out.printf("Restaurant Name: %s%nAddress %s%nCity %s, State %s, Zip %s", restaurantName, restAddy, restCity,restState, restZip);
+
+	
+    }
+} 
